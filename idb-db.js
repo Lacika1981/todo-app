b9dbd4090ccda6731f77a712bfa4550e36457042
Laszlo Varga <vargalaszlo1981@googlemail.com>
2020-02-14 11:35:19 +0000

    fixing grid layout, works on mobile, working on[ indexedDB, set and retrieve data to/from DB, not update view yet

--- idb-db.js
+++ idb-db.js
@@ -1,56 +1,109 @@
 import { openDB, deleteDB, wrap, unwrap } from 'https://unpkg.com/idb?module';
 
+import todoApp from './index.js';
 
 if (!window.indexedDB) {
-  window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+  window.alert(
+    "Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available."
+  );
 }
 
-// var data = [
-//   { uid: '12345', content: 'first content', done: 'false' },
-//   { uid: '67890', content: 'second content', done: 'false' },
-//   { uid: '13579', content: 'third content', done: 'true' }
-// ]
+var data = [
+  { uid: '12345', content: 'first content', done: 'false' },
+  { uid: '67890', content: 'second content', done: 'false' },
+  { uid: '13579', content: 'third content', done: 'true' },
+];
 
 var db;
 var request = window.indexedDB.open('todoApp', 1);
 
 request.onerror = function onerror(event) {
   console.log(`Ooops. Something went wrong. Error: ${event.target.errorCode}`);
-}
+};
 
 request.onsuccess = function onsuccess(event) {
   db = event.target.result;
-}
+  readFromDB();
+};
 
-request.onupgradeneeded = function (event) {
+request.onupgradeneeded = function(event) {
   db = event.target.result;
-  var objectStore = db.createObjectStore('todos', { keyPath : 'uid' });
+  console.log(db);
+  var objectStore = db.createObjectStore('todos', { keyPath: 'uid' });
   objectStore.createIndex('content', 'content', { unique: false });
   objectStore.createIndex('done', 'done', { unique: false });
-  objectStore.transaction.oncomplete = function (event) {
-    console.log('Object store created')
-    // var todoStore = db.transaction('todos', 'readwrite').objectStore('todos');
-    // data.forEach(function (todos) {
+  objectStore.transaction.oncomplete = function(event) {
+    console.log('Object store created');
+    var todoStore = db.transaction('todos', 'readwrite').objectStore('todos');
+    // data.forEach(function(todos) {
     //   todoStore.add(todos);
     // });
-  }
+  };
+};
+
+function readFromDB() {
+  console.log(db);
+  var transaction = db.transaction(['todos']);
+  var objectStore = transaction.objectStore('todos');
+
+  var request = objectStore.getAll();
+
+  console.log(request);
+  request.onerror = function(event) {
+    // Handle errors!
+  };
+  request.onsuccess = function(events) {
+    // Do something with the request.result!
+    console.log(events);
+    events.target.result.forEach(function(event) {
+      todoApp.createElems(event.content, event.uid, '');
+    });
+  };
 }
 
-document.querySelectorAll('.update_status').forEach( e => {
+export function addToDB(e) {
+  console.log(e.target);
+  var transaction = db.transaction(['todos'], 'readwrite');
+  var objectStore = transaction.objectStore('todos');
+  console.log(document.querySelector('input').value);
+  var objectStoreRequest = objectStore.add({
+    uid: todoApp.cretaeuid(),
+    content: this.closest('.add_text_field').children[1].value,
+  });
+
+  objectStoreRequest.onerror = function(event) {
+    console.log(event);
+    // Handle errors!
+  };
+  objectStoreRequest.transaction.oncomplete = function(event) {
+    // Do something with the request.result!
+    console.log(
+      `UID for content is: ${objectStoreRequest.result}
+	status: ${event.type}`
+    );
+  };
+}
+
+document.querySelectorAll('.update_status').forEach(e => {
   e.addEventListener('click', function(e) {
     console.log(e.target.closest('.content_container').children[1]);
-      var transaction = db.transaction(['todos'], 'readwrite');
-      var objectStore = transaction.objectStore('todos');
-      var objectStoreRequest = objectStore.add({'uid': e.target.closest('.content_container').getAttribute('data-uid'), 'content': e.target.closest('.content_container').children[1].textContent});
-      console.log(objectStoreRequest);
-    
-      objectStoreRequest.onerror = function(event) {
-        console.log(event);
-        // Handle errors!
-      };
-      objectStoreRequest.transaction.oncomplete = function(event) {
-        // Do something with the request.result!
-        console.log("UID for first content is: " + objectStoreRequest.result.content);
-      };
+    var transaction = db.transaction(['todos'], 'readwrite');
+    var objectStore = transaction.objectStore('todos');
+    var objectStoreRequest = objectStore.add({
+      uid: e.target.closest('.content_container').getAttribute('data-uid'),
+      content: e.target.closest('.content_container').children[1].textContent,
+    });
+    console.log(objectStoreRequest);
+
+    objectStoreRequest.onerror = function(event) {
+      console.log(event);
+      // Handle errors!
+    };
+    objectStoreRequest.transaction.oncomplete = function(event) {
+      // Do something with the request.result!
+      console.log(
+        'UID for first content is: ' + objectStoreRequest.result.content
+      );
+    };
   });
 });
--- index.js
+++ index.js
@@ -1,219 +1,234 @@
-var todoApp = (function todoApp() {
+import { addToDB } from './idb-db.js';
 
+var todoApp = function todoApp() {
+  var publicMethods = {
+    logState,
+    cretaeuid,
+    createElems,
+  };
 
-    var publicMethods = {
-        logState,
-    };
+  var state = {
+    text: '',
+    numberOfElems: 1,
+    updateContent: '',
+  };
 
-    var state = {
-        text: '',
-        numberOfElems: 1,
-        updateContent: '',
-    };
+  var storeName = 'todoApp';
 
-    var storeName = 'todoApp';
+  var inputField = document.querySelector('input.text');
+  var contentMain = document.querySelector('.content');
+  var addButton = document.querySelector('button.add_content');
 
-    var inputField = document.querySelector('input.text');
-    var contentMain = document.querySelector('.content');
-    var addButton = document.querySelector('button.add_content');
+  inputField.addEventListener('keyup', updateState);
 
-    inputField.addEventListener('keyup', updateState);
+  function logState() {
+    console.log({ state });
+  }
 
-    function logState() {
-        console.log({ state });
+  function updateState() {
+    state.text = inputField.value;
+    state.text != ''
+      ? addButton.removeAttribute('disabled')
+      : addButton.setAttribute('disabled', true);
+  }
+
+  function addMethod() {
+    addButton.addEventListener('click', createElems);
+    // addButton.addEventListener('click', addToDB);
+  }
+
+  function cretaeuid() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
+
+  /**
+   *
+   * @param {content that comes back from localStorage} t
+   * @param {id of each element stored in localStorage} id
+   */
+
+  function createElems(t, id, done) {
+    var text;
+    if (typeof t == 'string') {
+      text = t;
+    }
+    var content;
+    var contentContainer = document.createElement('section');
+    var listNumber = document.createElement('div');
+    var todoText = document.createElement('div');
+    var buttonContainer = document.createElement('div');
+    var doneButton = document.createElement('button');
+    var updateButton = document.createElement('button');
+    var deleteButton = document.createElement('button');
+    contentContainer.setAttribute('class', 'content_container');
+    listNumber.setAttribute('class', 'list_numbers');
+    todoText.setAttribute('class', 'todo_text');
+    todoText.setAttribute('contentEditable', false);
+    buttonContainer.setAttribute('class', 'button_container');
+    doneButton.setAttribute('class', 'update_status');
+    updateButton.setAttribute('class', 'update_done');
+    deleteButton.setAttribute('class', 'remove_content');
+    deleteButton.addEventListener('click', removeTodo);
+    updateButton.addEventListener('click', updateContent);
+    doneButton.addEventListener('click', doneContent);
+
+    content = document.createTextNode(text || state.text);
+
+    todoText.appendChild(content);
+
+    var uid = cretaeuid();
+    if (!text) {
+      addItemToStorage(uid, content, false);
+
+      contentContainer.setAttribute('data-uid', uid);
+    } else {
+      contentContainer.setAttribute('data-uid', id);
     }
 
+    var liNumber = document.createTextNode(state.numberOfElems);
+    listNumber.appendChild(liNumber);
 
-    function updateState() {
-        state.text = inputField.value;
-        state.text != '' ? addButton.removeAttribute('disabled') : addButton.setAttribute('disabled', true)
+    buttonContainer.appendChild(doneButton);
+    buttonContainer.appendChild(updateButton);
+    buttonContainer.appendChild(deleteButton);
+
+    contentContainer.appendChild(listNumber);
+    contentContainer.appendChild(todoText);
+    contentContainer.appendChild(buttonContainer);
+
+    if (done) {
+      doneButton.click();
     }
 
-    function addMethod() {
-        addButton.addEventListener('click', createElems);
+    contentMain.appendChild(contentContainer);
+
+    state.numberOfElems++;
+    inputField.value = '';
+    updateState();
+  }
+
+  function updateListNumbers() {
+    var liNumbers = document.querySelectorAll('.list_numbers');
+    for (var i = 1; i <= liNumbers.length; i++) {
+      liNumbers[i - 1].textContent = i;
     }
+  }
 
-    function cretaeuid() {
-        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-        )
+  function updateContent() {
+    var textNode = this.closest('.content_container').children[1];
+    var uid = this.closest('.content_container').getAttribute('data-uid');
+    state.updateContent = textNode.textContent;
+    textNode.getAttribute('contentEditable') == 'true'
+      ? (textNode.setAttribute('contentEditable', false),
+        updateStore(textNode.textContent, uid))
+      : (textNode.setAttribute('contentEditable', true), textNode.focus());
+  }
+
+  function doneContent() {
+    var textNode = this.closest('.content_container').children[1];
+    var uid = this.closest('.content_container').getAttribute('data-uid');
+    if (textNode.getAttribute('done') == 'true') {
+      return;
+    } else {
+      textNode.setAttribute('done', true);
+      textNode.setAttribute('contentEditable', false);
+      disableButton(
+        this.closest('.content_container').children[2].childNodes[1],
+        this
+      );
+      updateStoreIfContentDone(uid);
     }
+    textNode.style.textDecoration = 'line-through';
+  }
 
-    /**
-     * 
-     * @param {content that comes back from localStorage} t 
-     * @param {id of each element stored in localStorage} id 
-     */
+  function disableButton(...buttons) {
+    buttons.map(button => {
+      button.setAttribute('disabled', true);
+    });
+  }
 
-    function createElems(t, id, done) {
-        var text;
-        if (typeof t == 'string') {
-            text = t;
-        }
-        var content;
-        var contentContainer = document.createElement('section');
-        var listNumber = document.createElement('div');
-        var todoText = document.createElement('div');
-        var buttonContainer = document.createElement('div');
-        var doneButton = document.createElement('button');
-        var updateButton = document.createElement('button');
-        var deleteButton = document.createElement('button');
-        contentContainer.setAttribute('class', 'content_container');
-        listNumber.setAttribute('class', 'list_numbers');
-        todoText.setAttribute('class', 'todo_text');
-        todoText.setAttribute('contentEditable', false);
-        buttonContainer.setAttribute('class', 'button_container');
-        doneButton.setAttribute('class', 'update_status');
-        updateButton.setAttribute('class', 'update_done');
-        deleteButton.setAttribute('class', 'remove_content');
-        deleteButton.addEventListener('click', removeTodo);
-        updateButton.addEventListener('click', updateContent);
-        doneButton.addEventListener('click', doneContent);
+  function removeTodo() {
+    var toDelete = this.closest('.content_container');
+    var uid = toDelete.getAttribute('data-uid');
 
-        content = document.createTextNode(text || state.text);
+    deleteItemFromStorage(uid);
 
-        todoText.appendChild(content);
+    contentMain.removeChild(toDelete);
+    updateListNumbers();
+    state.numberOfElems--;
+  }
 
-        var uid = cretaeuid();
-        if (!text) {
-            addItemToStorage(uid, content, false);
-            contentContainer.setAttribute('data-uid', uid);
-        } else {
-            contentContainer.setAttribute('data-uid', id);
-        }
+  function retrieveStore(store) {
+    var storage = localStorage.getItem(store);
+    return JSON.parse(storage);
+  }
 
-        var liNumber = document.createTextNode(state.numberOfElems);
-        listNumber.appendChild(liNumber);
+  function setStore(store, array) {
+    localStorage.setItem(store, array);
+  }
 
-        buttonContainer.appendChild(doneButton);
-        buttonContainer.appendChild(updateButton);
-        buttonContainer.appendChild(deleteButton);
-
-        contentContainer.appendChild(listNumber);
-        contentContainer.appendChild(todoText);
-        contentContainer.appendChild(buttonContainer);
-
-        if(done) {
-            doneButton.click();
-        }
-
-        contentMain.appendChild(contentContainer);
-
-        state.numberOfElems++;
-        inputField.value = '';
-        updateState();
+  /**
+   *
+   * @param {unique ID from createuid function} uid
+   * @param {text content} content
+   * @param {whether the content is done or not (line through), if done can not be updated anymore} done
+   */
+  function addItemToStorage(uid, content, done) {
+    if (localStorage && !localStorage.getItem(storeName)) {
+      var elemsArray = [];
+      elemsArray.push({ uid, content: content.data, done });
+      setStore(storeName, JSON.stringify(elemsArray));
+    } else if (localStorage && localStorage.getItem(storeName)) {
+      var storeArray = retrieveStore(storeName);
+      storeArray.push({ uid, content: content.data, done });
+      setStore(storeName, JSON.stringify(storeArray));
     }
+  }
 
-    function updateListNumbers() {
-        var liNumbers = document.querySelectorAll('.list_numbers');
-        for (var i = 1; i <= liNumbers.length; i++) {
-            liNumbers[i - 1].textContent = i;
-        }
-    }
+  function deleteItemFromStorage(uid) {
+    var storeArray = retrieveStore(storeName);
+    storeArray = storeArray.filter(elem => {
+      return elem.uid != uid;
+    });
+    setStore(storeName, JSON.stringify(storeArray));
+  }
 
-    function updateContent() {
-        var textNode = this.closest('.content_container').children[1];
-        var uid = this.closest('.content_container').getAttribute('data-uid');
-        state.updateContent = textNode.textContent;
-        textNode.getAttribute('contentEditable') == 'true'
-            ? ( textNode.setAttribute('contentEditable', false), updateStore(textNode.textContent, uid) )
-            : ( textNode.setAttribute('contentEditable', true), textNode.focus() );
-    }
+  function updateStore(content, uid) {
+    var storeArray = retrieveStore(storeName);
+    storeArray.find(elem => {
+      if (elem.uid == uid) {
+        return (elem.content = content);
+      }
+    });
+    setStore(storeName, JSON.stringify(storeArray));
+  }
 
-    function doneContent() {
-        var textNode = this.closest('.content_container').children[1];
-        var uid = this.closest('.content_container').getAttribute('data-uid');
-        if (textNode.getAttribute('done') == 'true') {
-            return
-        } else {
-            textNode.setAttribute('done', true);
-            textNode.setAttribute('contentEditable', false);
-            disableButton(this.closest('.content_container').children[2].childNodes[1], this);
-            updateStoreIfContentDone(uid);
-        }
-        textNode.style.textDecoration = 'line-through'
-    }
+  function updateStoreIfContentDone(uid) {
+    var storeArray = retrieveStore(storeName);
+    storeArray.find(elem => {
+      if (elem.uid == uid) {
+        return (elem.done = true);
+      }
+    });
+    setStore(storeName, JSON.stringify(storeArray));
+  }
 
-    function disableButton(...buttons) {
-        buttons.map(button => {
-            button.setAttribute('disabled', true);
-        })
-    }
+  function restoreList() {
+    var storeArray = retrieveStore(storeName);
+    if (storeArray)
+      storeArray.map(elem => createElems(elem.content, elem.uid, elem.done));
+  }
 
-    function removeTodo() {
-        var toDelete = this.closest('.content_container');
-        var uid = toDelete.getAttribute('data-uid');
+  restoreList();
 
-        deleteItemFromStorage(uid);
+  addMethod();
 
-        contentMain.removeChild(toDelete);
-        updateListNumbers();
-        state.numberOfElems--;
-    }
+  return publicMethods;
+};
 
-    function retrieveStore(store) {
-        var storage = localStorage.getItem(store);
-        return JSON.parse(storage);
-    }
-
-    function setStore(store, array) {
-        localStorage.setItem(store, array);
-    }
-
-
-    /**
-     * 
-     * @param {unique ID from createuid function} uid 
-     * @param {text content} content 
-     * @param {whether the content is done or not (line through), if done can not be updated anymore} done 
-     */
-    function addItemToStorage(uid, content, done) {
-        if (localStorage && !localStorage.getItem(storeName)) {
-            var elemsArray = [];
-            elemsArray.push({ uid, content: content.data, done });
-            setStore(storeName, JSON.stringify(elemsArray));
-        }
-        else if (localStorage && localStorage.getItem(storeName)) {
-            var storeArray = retrieveStore(storeName);
-            storeArray.push({ uid, content: content.data, done });
-            setStore(storeName, JSON.stringify(storeArray));
-        }
-    }
-
-    function deleteItemFromStorage(uid) {
-        var storeArray = retrieveStore(storeName);
-        storeArray = storeArray.filter(elem => { return elem.uid != uid });
-        setStore(storeName, JSON.stringify(storeArray));
-    }
-
-    function updateStore(content, uid) {
-        var storeArray = retrieveStore(storeName);
-        storeArray.find(elem => {
-            if (elem.uid == uid) {
-                return elem.content = content;
-            }
-        });
-        setStore(storeName, JSON.stringify(storeArray));
-    }
-
-    function updateStoreIfContentDone(uid) {
-        var storeArray = retrieveStore(storeName);
-        storeArray.find(elem => {
-            if (elem.uid == uid) {
-                return elem.done = true;
-            }
-        });
-        setStore(storeName, JSON.stringify(storeArray));
-    }
-
-    function restoreList() {
-        var storeArray = retrieveStore(storeName);
-        if (storeArray) storeArray.map(elem => createElems(elem.content, elem.uid, elem.done));
-    }
-
-    restoreList();
-
-    addMethod();
-
-    return publicMethods;
-})
+export default todoApp();
